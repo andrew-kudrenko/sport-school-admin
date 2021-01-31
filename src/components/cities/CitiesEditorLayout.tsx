9f4bb94bcc46ba1017750e5f4d9d22c0f3652e7b
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2021-01-31 20:38:30 +0300

    [addded] the field for a city\'s token

--- src/components/cities/CitiesEditorLayout.tsx
+++ src/components/cities/CitiesEditorLayout.tsx
@@ -1,65 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import { Grid, TextField } from '@material-ui/core'
-import { useFormHandlers } from '../../hooks/form-handlers.hooks'
-import { IEntityEditorProps } from '../../interfaces/components.interfaces'
-import { EditorFormLayout } from '../layouts/EditorFormLayout'
-import { useIDParam } from '../../hooks/id-param.hook'
-import { useFoundCities } from '../../hooks/found-by-city.hook'
-import { useDeleteQuery, usePostQuery, usePutQuery } from '../../hooks/query.hook'
-import { validate } from '../../helpers/truthy-validator.helper'
+import React, { useEffect, useState } from "react";
+import { Grid, TextField } from "@material-ui/core";
+import { useFormHandlers } from "../../hooks/form-handlers.hooks";
+import { IEntityEditorProps } from "../../interfaces/components.interfaces";
+import { EditorFormLayout } from "../layouts/EditorFormLayout";
+import { useIDParam } from "../../hooks/id-param.hook";
+import { useFoundCities } from "../../hooks/found-by-city.hook";
+import {
+  useDeleteQuery,
+  usePostQuery,
+  usePutQuery,
+} from "../../hooks/query.hook";
+import { validate } from "../../helpers/truthy-validator.helper";
 
-export const CitiesEditorLayout: React.FC<IEntityEditorProps> = ({ mode, title }) => {
-    const editing = mode === 'edit'
-    
-    const id = useIDParam()
-    let { cities, loading, execute: fetchCities } = useFoundCities()
-    
-    const [name, setName] = useState('')
-    
-    const { onChange } = useFormHandlers()
-    const onClearAll = setName.bind(null, '')
+export const CitiesEditorLayout: React.FC<IEntityEditorProps> = ({
+  mode,
+  title,
+}) => {
+  const editing = mode === "edit";
 
-    const { execute: onAdd, loading: adding } = usePostQuery('structures/cities', { name })
-    const { execute: onModify, loading: modifying } = usePutQuery(`structures/cities/${id}`, { name })
-    const { execute: onRemove, loading: removing } = useDeleteQuery(`structures/cities/${id}`)
+  const id = useIDParam();
+  let { cities, loading, execute: fetchCities } = useFoundCities();
 
-    const isValid = validate([name])
+  const [name, setName] = useState("");
+  const [payments, setPayments] = useState("");
 
-    useEffect(() => {
-        const city = cities?.find(c => String(c.id) === id)
+  const { onChange } = useFormHandlers();
+  const onClearAll = setName.bind(null, "");
 
-        if (editing && city) {
-            setName(city.name)    
-        }
-    }, [cities])
+  const { execute: onAdd, loading: adding } = usePostQuery(
+    "structures/cities",
+    { name, payments }
+  );
+  const {
+    execute: onModify,
+    loading: modifying,
+  } = usePutQuery(`structures/cities/${id}`, { name, payments });
+  const { execute: onRemove, loading: removing } = useDeleteQuery(
+    `structures/cities/${id}`
+  );
 
-    return (
-        <EditorFormLayout
-            mode={mode}
-            isValid={isValid}
-            redirectTo="/cities/"
-            title={title}
-            loading={{ read: loading, create: adding, update: modifying, delete: removing}}
-            onAdd={onAdd}
-            onClearAll={onClearAll}
-            onRemove={async () => {
-                await onRemove()
-                await fetchCities()
-            }}
-            onModify={onModify}
-        >
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        fullWidth
-                        label="Название"
-                        autoFocus
-                        value={name}
-                        onChange={onChange(setName)}
-                    />
-                </Grid>
-            </Grid>
-        </EditorFormLayout>
-    )
-}
+  const isValid = validate([name]);
+
+  useEffect(() => {
+    const city = cities?.find((c) => String(c.id) === id);
+
+    if (editing && city) {
+      setName(city.name);
+      setPayments(city.payments);
+    }
+  }, [cities]);
+
+  return (
+    <EditorFormLayout
+      mode={mode}
+      isValid={isValid}
+      redirectTo="/cities/"
+      title={title}
+      loading={{
+        read: loading,
+        create: adding,
+        update: modifying,
+        delete: removing,
+      }}
+      onAdd={onAdd}
+      onClearAll={onClearAll}
+      onRemove={async () => {
+        await onRemove();
+        await fetchCities();
+      }}
+      onModify={onModify}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            label="Название"
+            autoFocus
+            value={name}
+            onChange={onChange(setName)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            label="Токен"
+            value={payments}
+            onChange={onChange(setPayments)}
+          />
+        </Grid>
+      </Grid>
+    </EditorFormLayout>
+  );
+};
--- src/interfaces/entities.interfaces.ts
+++ src/interfaces/entities.interfaces.ts
@@ -1,185 +1,186 @@
-export type IDType = string
+export type IDType = string;
 
-export type ErrorType = string | null
+export type ErrorType = string | null;
 
 export interface IdentifiedEntity {
-  id: IDType
+  id: IDType;
 }
 
 export interface ICity extends INonIDCity {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDCity {
-  id?: IDType | undefined
-  name: string
+  id?: IDType | undefined;
+  name: string;
+  payments: string;
 }
 
 export interface ITransaction extends INonIDTransaction {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDTransaction {
-  id?: IDType
-  title: string
+  id?: IDType;
+  title: string;
 }
 
 export interface ISchool extends INonIDSchool {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDSchool {
-  id?: IDType
-  name: string
-  address: string
-  description: string
-  city_id: IDType
+  id?: IDType;
+  name: string;
+  address: string;
+  description: string;
+  city_id: IDType;
 }
 
 export interface IGroup extends INonIDGroup {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDGroup {
-  id?: IDType
-  year: number
-  schedule: string
-  tg_url: string
-  school_id: IDType
-  trainer_ids: Array<IDType>
+  id?: IDType;
+  year: number;
+  schedule: string;
+  tg_url: string;
+  school_id: IDType;
+  trainer_ids: Array<IDType>;
 }
 
 export interface ICoach extends INonIDGroup {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDCoach {
-  id?: IDType
+  id?: IDType;
 }
 
 export interface INews extends INonIDNews {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDNews {
-  id?: IDType
-  title: string
-  author_id: string
-  text: string
-  img?: any
+  id?: IDType;
+  title: string;
+  author_id: string;
+  text: string;
+  img?: any;
 }
 
 export interface ICoach extends INonIDCoach {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDCoach {
-  id?: IDType
-  name: string
-  user_id: string
-  dob: string
-  description: string
-  address: string
-  tel: string
-  group_id: Array<IDType>
-  city_id: IDType
+  id?: IDType;
+  name: string;
+  user_id: string;
+  dob: string;
+  description: string;
+  address: string;
+  tel: string;
+  group_id: Array<IDType>;
+  city_id: IDType;
 }
 
 export interface INonIDUser {
-  id?: IDType
-  login: string
-  name: string
-  is_trainer: boolean
-  is_child: boolean
-  is_verify: boolean
-  was_activate: boolean
-  city_id: IDType
-  address: string
-  tel: string
-  date_joined: string
-  school_id: IDType
+  id?: IDType;
+  login: string;
+  name: string;
+  is_trainer: boolean;
+  is_child: boolean;
+  is_verify: boolean;
+  was_activate: boolean;
+  city_id: IDType;
+  address: string;
+  tel: string;
+  date_joined: string;
+  school_id: IDType;
 }
 
 export interface IStudent extends INonIDStudent {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDStudent {
-  id?: IDType
-  img: string
-  name: string
-  dob: string
-  address: string
-  group_id: IDType
-  parents_ids: Array<IDType>
+  id?: IDType;
+  img: string;
+  name: string;
+  dob: string;
+  address: string;
+  group_id: IDType;
+  parents_ids: Array<IDType>;
 }
 
 export interface INonIDStatistics {
-  id?: IDType
-  children_id: string
-  date: string
-  file: string
+  id?: IDType;
+  children_id: string;
+  date: string;
+  file: string;
 }
 
 export interface IStatistics extends INonIDStatistics {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDTournament {
-  id?: IDType
-  author_id: IDType
-  text: string
-  year: number
-  img: string
+  id?: IDType;
+  author_id: IDType;
+  text: string;
+  year: number;
+  img: string;
 }
 
 export interface ITournament extends INonIDTournament {
-  id: IDType
+  id: IDType;
 }
 
 export interface IStatistics extends INonIDStatistics {
-  id: IDType
+  id: IDType;
 }
 
 export interface IUser extends INonIDUser {
-  id: IDType
+  id: IDType;
 }
 
 export interface INonIDUser {
-  id?: IDType
-  login: string
-  name: string
-  is_trainer: boolean
-  is_child: boolean
-  is_verify: boolean
-  was_activate: boolean
-  city_id: IDType
-  address: string
-  tel: string
-  date_joined: string
-  school_id: IDType
-  childs_id: Array<IDType>
-  tg_id?: IDType
+  id?: IDType;
+  login: string;
+  name: string;
+  is_trainer: boolean;
+  is_child: boolean;
+  is_verify: boolean;
+  was_activate: boolean;
+  city_id: IDType;
+  address: string;
+  tel: string;
+  date_joined: string;
+  school_id: IDType;
+  childs_id: Array<IDType>;
+  tg_id?: IDType;
 }
 
 export interface ILoginCredentials {
-    login: string
-    password: string
+  login: string;
+  password: string;
 }
 
 export interface ICachedUserData {
-  login: string
-  token: string
+  login: string;
+  token: string;
 }
 
-export type TokenType = string
+export type TokenType = string;
 
-export type ID<T extends object> = { id: IDType } & { [K in keyof T]: T[K] } 
+export type ID<T extends object> = { id: IDType } & { [K in keyof T]: T[K] };
 
 export interface ILog {
-  is_add: Boolean
-  cost: number
-  text: string
-  user_id: IDType
-  type_transaction_id: IDType
+  is_add: Boolean;
+  cost: number;
+  text: string;
+  user_id: IDType;
+  type_transaction_id: IDType;
 }
